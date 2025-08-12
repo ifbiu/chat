511d13b9a2b58855cd018d19dc12eb5d2e91428e
Candide <biuvip@163.com>
2025-08-12 23:41:30 +0800

    add: github ci

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -19,12 +19,14 @@ use axum::{
     routing::{get, patch, post},
     Router,
 };
+use axum::handler::Handler;
+use axum::middleware::from_fn_with_state;
 use tower_http::compression::CompressionLayer;
 use tower_http::LatencyUnit;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tracing::Level;
 pub use config::AppConfig;
-use crate::middlewares::set_layer;
+use crate::middlewares::{set_layer, verify_token};
 
 #[derive(Debug, Clone)]
 pub(crate) struct AppState {
@@ -43,8 +45,6 @@ pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
     let state = AppState::try_new(config).await?;
 
     let api = Router::new()
-        .route("/signin", post(signin_handler))
-        .route("/signup", post(signup_handler))
         .route("/chat", get(list_chat_handler).post(create_chat_handler))
         .route(
             "/chat/:id",
@@ -52,14 +52,18 @@ pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
                 .delete(delete_chat_handler)
                 .post(send_message_handler),
         )
-        .route("/chat/:id/messages", get(list_message_handler));
+        .route("/chat/:id/messages", get(list_message_handler))
+        .layer(from_fn_with_state(state.clone(), verify_token))
+        // routes doesn't need token verification
+        .route("/signin", post(signin_handler))
+        .route("/signup", post(signup_handler));
 
     let app = Router::new()
         .route("/", get(index_handler))
         .nest("/api", api)
-        .with_state(state);
+        .with_state(state.clone());
 
-    Ok(set_layer(app))
+    Ok(set_layer(app, state))
 }
 
 // 当我调用 state.config => state.inner.config
--- chat_server/src/middlewares/mod.rs
+++ chat_server/src/middlewares/mod.rs
@@ -1,10 +1,14 @@
-use axum::middleware::from_fn;
+use axum::extract::State;
+use axum::middleware::{from_fn, from_fn_with_state};
 use axum::Router;
+use tower::layer::util::Identity;
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::LatencyUnit;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tracing::Level;
+use tracing_subscriber::fmt::layer;
+use crate::AppState;
 use crate::middlewares::request_id::set_request_id;
 use crate::middlewares::server_time::ServerTimeLayer;
 
@@ -12,11 +16,14 @@ mod request_id;
 mod server_time;
 mod auth;
 
+pub use auth::verify_token;
+
 const REQUEST_ID_HEADER: &str = "x-request-id";
 const SERVER_TIME_HEADER: &str = "x-server-time";
 
-pub fn set_layer(app: Router) -> Router {
+pub fn set_layer(app: Router, state: AppState) -> Router {
     app.layer(ServiceBuilder::new()
+         .layer(from_fn_with_state(state,verify_token))
         .layer(TraceLayer::new_for_http()
             .make_span_with(DefaultMakeSpan::new().include_headers(true))
             .on_request(DefaultOnRequest::new().level(Level::INFO))
